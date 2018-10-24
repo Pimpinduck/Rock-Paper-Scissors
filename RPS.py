11f7ef11342c39a38dc434d897e1319ed3f89c43
Justin McCabe <43969246+Pimpinduck@users.noreply.github.com>
2018-10-24 15:09:17 -0400

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,15 @@
+import sys
+import random
+
+yn = y
+
+print('Welcome to Rock Paper Scissors!')
+while (yn != n):
+    choice = input("Enter R(rock), P(paper), or S(scissors): ")
+    print("Rock!")
+    print("Paper!")
+    print("Scissors!")
+    print("Shoot!")
+    
+    
+
