9fa977858133e077310b6b138743900a5e950e82
Hayden Riewe <hriewe@186.129.21.198.guest.wifi.dyn.clemson.edu>
2018-11-09 15:08:43 -0500

    Added scoreboard and timer

--- RPS.py
+++ RPS.py
@@ -3,6 +3,9 @@ import random
 import time
 
 yn = "y"
+gameCount = 1
+playerWin = 0
+compWin = 0
 
 print('Welcome to Rock Paper Scissors!')
 while (yn != "n" and yn != "N"): #Starting off the loop
@@ -14,6 +17,7 @@ while (yn != "n" and yn != "N"): #Starting off the loop
     print("Scissors!")
     time.sleep(1)
     print("Shoot!")
+    time.sleep(1)
     randnum = random.randint(1, 3) #Choosing a random choice for the computer
     if choice == "R" or choice == "r": # If statements to determine what should be displayed to the user at the end
         choiceS = "Rock"
@@ -23,33 +27,43 @@ while (yn != "n" and yn != "N"): #Starting off the loop
         if randnum == 2:
             result = "Loss"
             compChoice = "Paper"
+            compWin += 1
         if randnum == 3:
             result = "Win"
             compChoice = "Scissors"
+            playerWin += 1
     if choice == "P" or choice == "p": #Added more user options
         choiceS = "Paper"
         if randnum == 1:
             result = "Win"
             compChoice = "Rock"
+            playerWin += 1
         if randnum == 2:
             result = "Tie"
             compChoice = "Paper"
         if randnum == 3:
             result = "Loss"
             compChoice = "Scissors"
+            compWin += 1
     if choice == "S" or choice == "s": #Added more user options
         choiceS = "Scissors"
         if randnum == 1:
             result = "Loss"
             compChoice = "Rock"
+            compWin += 1
         if randnum == 2:
             result = "Win"
             compChoice = "Paper"
+            playerWin += 1
         if randnum == 3:
             result = "Tie"
             compChoice = "Scissors"
-    print("You chose " + choiceS + " and the computer chose " + compChoice) #showing the result of the game to the user
-    print("The result is a " + result + "!")  #Printing result absolutely
+    print("\nYou chose " + choiceS + " and the computer chose " + compChoice + "\n") #showing the result of the game to the user
+    print("The result is a " + result + "!\n")  #Printing result absolutely
+    print("The current standings are:")
+    print("You: " + str(playerWin) + "/" + str(gameCount))
+    print("Computer: " + str(compWin) + "/" + str(gameCount) + "\n")
+    gameCount += 1
     yn = input("Would you like to play again? (y or n): ") #Play again Y or N
     
 
